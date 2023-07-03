239bf7be23d28816c760ae0ddb7542280c32dc5f
Emiliano A. Billi <emiliano.billi@gmail.com>
2023-07-03 10:41:06 -0300

    Update pkst_mediainfo.c

--- src/pkst_mediainfo.c
+++ src/pkst_mediainfo.c
@@ -208,6 +208,9 @@ cleanup:
         avformat_close_input(&pFormatContext);
         avformat_free_context(pFormatContext);
     }
+    if (avioBuffer != NULL) {
+        av_free(avioBuffer);
+    }
     return ret;
 }
 
