c8ea6c7fb564055c7fcbb24e076f5fe420ee5b33
Emiliano A. Billi <emiliano.billi@gmail.com>
2025-01-13 22:47:06 -0300

    Update pkst_iocontext.c

--- src/pkst_iocontext.c
+++ src/pkst_iocontext.c
@@ -823,7 +823,7 @@ int pkst_process_av_packet(PKSTts *ts, AVPacket *pkt, PKSTInputCtx *in, PKSTMult
             ts->start_dts = pkt->dts;
         }
         pkt->pts = pkt->pts - ts->start_pts;
-        pkt->dts = pkt->dts - ts->start_pts;
+        pkt->dts = pkt->dts - ts->start_dts;
         if (pkt->stream_index == in->streams[PKST_AUDIO_STREAM_OUTPUT]) {
             error = pkst_process_audio_packet(pkt, in, out, output_fail);
         } else {
